b7616dfcca0693793d2b92de4118b180cf09a22f
Eirren <edv2019@gmail.com>
2026-04-08 12:01:40 -0700

    Added increasing and decreasing friendship level to menu, made menu loop until exit

--- Lab27.cpp
+++ Lab27.cpp
@@ -45,16 +45,49 @@ int main() {
 
 // menu function for scalable menu
 void menu(map<string, tuple<int, string, string>> v) {
+    // int to contain user menu choice
+    int choice = -1;
+
     cout << "1. Increase Friendship" << endl
          << "2. Decrease Friendship" << endl
          << "3. Search For Villager" << endl
          << "4. Exit" << endl;
-
-    // moving villager printing into menu
-    for (auto pair : v) {
-        cout << pair.first;
-        auto [villagerLevel, villagerSpecies, villagerPhrase] = pair.second;
-        cout << " [" << villagerLevel << ", " << villagerSpecies << ", " << villagerPhrase << "]";
-        cout << endl;
+    cin >> choice;
+    while (choice != 4) {
+        while (choice < 1 || choice > 4) {
+            cout << "Please input a choice between 1 to 4:" << endl;
+            cout << "1. Increase Friendship" << endl
+            << "2. Decrease Friendship" << endl
+            << "3. Search For Villager" << endl
+            << "4. Exit" << endl;
+            cin >> choice;
+            cout << endl;
+        }
+        if (choice == 1) {
+            for (auto &pair : v) {
+            if (get<0>(pair.second) != 10) {
+                    get<0>(pair.second) += 1;
+            }
+            }
+        }
+        else if (choice == 2) {
+            for (auto &pair : v) {
+            if (get<0>(pair.second) != 0) {
+                    get<0>(pair.second) -= 1;
+            }
+            }
+        }
+        // moving villager printing into menu
+        cout << "Villager details:" << endl;
+        for (auto pair : v) {
+            cout << pair.first;
+            auto [villagerLevel, villagerSpecies, villagerPhrase] = pair.second;
+            cout << " [" << villagerLevel << ", " << villagerSpecies << ", " << villagerPhrase << "]";
+            cout << endl;
+        }
+        cin >> choice;
     }
+
+
+    
 }
